a28c0a9df16f180a9fbc81c28dfab5f50f42ebd5
satellez <santiagoandres0226@hotmail.es>
2024-10-01 18:40:04 -0500

    New changes, in program and relationships

--- WebApplication1AGRO/Program.cs
+++ WebApplication1AGRO/Program.cs
@@ -61,11 +61,10 @@ builder.Services.AddScoped<IPaymentMethodsService, PaymentMethodsService>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
+
     app.UseSwagger();
     app.UseSwaggerUI();
-}
+
 
 app.UseHttpsRedirection();
 
